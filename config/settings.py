ad6d4a9558ebc9a7aa3dfdd614c43d8274b02e06
Abdumajid7 <jackodilov@gmail.com>
2023-09-28 17:15:32 +0500

    added i18 settings

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -79,9 +80,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us' #default
 
-TIME_ZONE = 'UTC'
+LANGUAGES = (
+    ('en', 'us'),
+    ('uz', 'uz'),
+    ('ru', 'ru'),
+)
+
+TIME_ZONE = 'Asia/Tashkent'
+
+LOCALE_PATHS = BASE_DIR / 'locale',
 
 USE_I18N = True
 
--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('main.urls')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('users.urls')),
+    path('', include('main.urls')),
+)
 
 urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)

c70ad6dae2eb659625117f91d3d8e6f717d2b6fb
Abraham <jayson060702@gmail.com>
2023-10-12 17:16:31 +0500

    login function added

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 # users/views.py
 
 from django.shortcuts import render, redirect
-from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
+from .forms import RegistrationForm, LoginForm
+
 
 def register(request):
     if request.method == "POST":
@@ -12,3 +14,22 @@ def register(request):
     else:
         form = RegistrationForm()
     return render(request, 'auth/sign-up.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a specific page after login, e.g., the user's profile
+                return redirect('main:home')
+            else:
+                # Invalid login
+                form.add_error(None, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'auth/sign-in.html', {'form': form})

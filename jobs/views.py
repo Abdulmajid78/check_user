b54499eef784b09f9c5a9296560c7c0ec33fe3fc
Abdumajid7 <jackodilov@gmail.com>
2023-11-10 17:37:59 +0500

    nav almost fixed

--- jobs/views.py
+++ jobs/views.py
@@ -81,9 +81,9 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
 
 class CompanyListView(ListView):
-    model = CompanyModel
     template_name = 'jobs/companies-list.html'
-    paginate_by = 1
+    paginate_by = 4
+    model = CompanyModel
 
     def get_context_data(self, *, object_list=None, **kwargs):
         data = super().get_context_data()
@@ -111,9 +111,6 @@ class CandidatesView(TemplateView):
 class PostJobView(TemplateView):
     template_name = 'post-job.html'
 
-
-
-
 # @require_POST
 # def employee_comment(request, employee_id):
 #     employee = get_object_or_404(EmployeeModel, id=employee_id, status=EmployeeModel)
@@ -131,7 +128,6 @@ class PostJobView(TemplateView):
 #                   {'employee': employee,
 #                    'form': form,
 #                    'comment': comment})
-
 
 
 # class AddReview(View):
